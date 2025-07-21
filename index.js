cfc7dc618680371cf153eca202f5b5fedb1c4933
Mohammed Khaleel Uddin <151870351+khaleeluddin9912@users.noreply.github.com>
2025-07-21 15:51:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline!');
+  res.send('Hello This is updated CI/CD Pipeline!');
 });
 
 app.listen(port, () => {
